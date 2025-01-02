a84d34e64ed71bb5b4a29c66b458e82e133e3f33
Haris Kljajic <haris@myrstack.tech>
2025-01-02 12:33:14 +0100

    feat: deploy pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/pianotopia",
+  output: "export",
+  reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com", "xsgames.co"],
   },
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -15,7 +15,10 @@ export default function Hero({ viewRef, scrollToBottom }: HeroProps) {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/piano.mp4" type="video/mp4" />
+        <source
+          src="https://github.com/fredagsfys/pianotopia/blob/main/public/piano.mp4"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
       <div
